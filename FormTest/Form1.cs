e0c14a88a925b7c53bbe9cdce92b8c90978adbc6
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 18:52:37 +0300

    Add project files.

---
+++ FormTest/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.PreviewKeyDown += Form1_PreviewKeyDown;
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    if (e.Control == true)
+                        MessageBox.Show("Select all");
+                    break;
+            }
+        }
+    }
+}
